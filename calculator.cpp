924cf973c51c5bcd35d4fad7cda10e6351aad680
AntKost <antkost64@gmail.com>
2022-01-18 19:41:57 +0200

    Added division operation

--- calculator.cpp
+++ calculator.cpp
@@ -4,11 +4,18 @@ int Calculator::Add (double a, double b)
 {
     return a + b;
 }
+
 int Calculator::Sub (double a, double b)
 {
     return Add (a, -b);
 }
+
+int Calculator::Div (double a, double b)
+{
+    return a / b; 
+}
+
 int Calculator::Mul (double a, double b)
 {
-    return a * b + 0.5;
+    return a * b;
 }
